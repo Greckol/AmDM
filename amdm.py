3ce0c8f597916b8ec92e87ded694e12703dabc3d
Greck <vitaliachal@gmail.com>
2022-04-24 18:14:34 +0900

    version 1.0.1

--- amdm.py
+++ amdm.py
@@ -1,5 +1,4 @@
 import requests
-import os
 from bs4 import BeautifulSoup
 
 
@@ -26,18 +25,26 @@ def x():
         file_w.write(s[:-1])
 
 
-def amdm(first_page=1, last_page=2):
+def amdm(period="all", first_page=1, last_page=2):
+
+    if last_page > 10:
+        last_page = 10
+    if first_page < 1:
+        first_page = 1
+
     text_clean()
+    
     number = 0
     while first_page != last_page+1:
-        response = requests.get(f"https://amdm.ru/akkordi/popular/all/page{first_page}/")
+        response = requests.get(f"https://amdm.ru/akkordi/popular/{period}/page{first_page}/")
         soup = BeautifulSoup(response.content, "lxml")
         data = soup.find('table', {"class": "items"}).findAll(class_="artist_name")
         for curse in data:
             number += 1
             text_write(f"{number }  {curse.text}\n")
         first_page += 1
+
     x()
     print(text_read())
 
-amdm(int(input("first_page: ")), int(input("last_page: ")))
+amdm(input("period (month or all): "), int(input("first_page: ")), int(input("last_page: ")))
