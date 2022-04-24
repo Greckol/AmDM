69f0db03c4f40efe8582ac60e1b677df8c78bb4b
Greck <vitaliachal@gmail.com>
2022-04-24 15:33:54 +0900

    version 0.0.3

--- amdm.py
+++ amdm.py
@@ -2,16 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def text_clean():
+    with open("config.txt", "w") as file:
+        file.write("")
+
+
+def text(song):
+    with open("config.txt", "a") as file:
+        file.write(song)
+
+
 def amdm(firt_page=1, last_page=2):
-    count = 0
+    text_clean()
+    number = 0
     while firt_page != last_page+1:
         response = requests.get(f"https://amdm.ru/akkordi/popular/all/page{firt_page}/")
         soup = BeautifulSoup(response.content, "lxml")
         data = soup.find('table', {"class": "items"}).findAll(class_="artist_name")
         for curse in data:
-            count += 1
-            print(count, curse.text)
+            number += 1
+            text(f"{number }  {curse.text}  \n")
+            print(number, curse.text)
         firt_page += 1
 
 
-amdm()
+amdm(last_page=2)
